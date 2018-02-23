d449a0680085d1490937943a1d44d8cd4db231ba
eduardonunes <eduardonunes056@gmail.com>
2018-02-23 01:54:21 -0300

    removed React starter

--- src/Components/shared/Label/Label.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './Label.css';
-
-export const Label = props => (
-	<label className="float-label">{props.children}</label>
-);
-
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {person} from './person';
-import { reducer as form } from 'redux-form';
-import { routerReducer as  routing} from 'react-router-redux';
-export {
-	person, 
-	form,
-	routing
-};
-
